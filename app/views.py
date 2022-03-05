4d95dbc201542a64dbd712934456a75ea0f0a3fa
Animesh Risal <animeshrisal99@gmail.com>
2022-03-05 13:38:58 +0545

    Add more html content

--- app/views.py
+++ app/views.py
@@ -1,20 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Movie
+# from django.shortcuts import render
+# from .models import Movie
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World')
 
-def about(request):
-    return HttpResponse('About Page')
+# def about(request):
+#     return HttpResponse('About Page')
 
-def number(request, id):
-    return HttpResponse(id)
+# def number(request, id):
+#     return HttpResponse(id)
 
-def get_movie(request, id):
-    try:
-        movie = Movie.objects.get(pk=id)
-        return render(request, 'movie.html', {'movie': movie})
-    except Movie.DoesNotExist:
-        return render(request, '404.html')
+# def get_movie(request, id):
+#     try:
+#         movie = Movie.objects.get(pk=id)
+#         return render(request, 'movie.html', {'movie': movie})
+#     except Movie.DoesNotExist:
+#         return render(request, '404.html')
+
+def get_movies(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies.html', {'movies': movies})
