72d16884081389e7a708814d67789d436f5c67db
Animesh Risal <animeshrisal99@gmail.com>
2022-03-05 13:49:31 +0545

    Uncomment

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
-    # path('about/', views.about),
-    # path('number/<int:id>/', views.number),
-    # path('movies/<int:id>/', views.get_movie),
-    # path('movies/', views.get_movies)
+    path('about/', views.about),
+    path('number/<int:id>/', views.number),
+    path('movies/<int:id>/', views.get_movie),
+    path('movies/', views.get_movies)
 ]
