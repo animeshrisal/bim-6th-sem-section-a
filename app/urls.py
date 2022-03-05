4a99ebe9c4eae6281e2c4fdd1ba17cd09da2bbdd
Animesh Risal <animeshrisal99@gmail.com>
2022-03-05 13:22:06 +0545

    Add urls and html

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index),
+    path('about/', views.about),
+    path('number/<int:id>/', views.number),
+    path('movies/<int:id>/', views.get_movie)
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Hello World')
+
+def about(request):
+    return HttpResponse('About Page')
+
+def number(request, id):
+    return HttpResponse(id)
+
+def get_movie(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+        return render(request, 'movie.html', {'movie': movie})
+    except Movie.DoesNotExist:
+        return render(request, '404.html')
