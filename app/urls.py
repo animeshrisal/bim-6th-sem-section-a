90b1052162b9e451d2326f7f81b151417c0b58d3
Animesh Risal <animeshrisal99@gmail.com>
2022-03-21 14:34:06 +0545

    Add login

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('post_movie/', views.post_movie),
     path('update_movie/<int:id>/', views.update_movie),
     path('delete_movie/<int:id>/', views.delete_movie),
+    path('signup/', views.signup, name="User Sign Up"),
+    path('signin/', views.signin, name="User Sign In"),
+    path('signout/', views.signout, name="User Sign Out"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from app.forms import MovieForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import MovieForm
 from .models import Movie
 
 def get_movie(request, id):
@@ -50,7 +53,7 @@ def post_movie(request):
         if movie_form.is_valid():
             movie_form.save()
 
-            return redirect('/movies/page/1')
+            return redirect('/movies')
 
     return render(request, 'post_movie.html', {'movie_form': movie_form})
 
@@ -63,8 +66,8 @@ def update_movie(request, id):
             movie_form.save()
             return redirect('/movies/{}'.format(id))
 
-    elif request.method == "GET":
-        movie_form = MovieForm(instance=movie)
+ 
+    movie_form = MovieForm(instance=movie)
 
     return render(request, 'update_movie.html', {'form': movie_form})
 
@@ -72,3 +75,47 @@ def delete_movie(request, id):
     movie = Movie.objects.get(pk=id)
     movie.delete()
     return redirect('/movies/')
+
+def signin(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'], 
+                password=form.cleaned_data['password'])
+            login(request, user)
+            return redirect('/movies/')
+
+    return render(request, 'signin.html', {'form': form})
+
+def signup(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('/movies/')
+
+    return render(request, 'signup.html', {'form': form})
+
+def signout(request):
+    logout(request)
+    return redirect('/movies/')
+
+def add_to_favorite(request, id):
+    movie = Movie.objects.get(id=id)
+    movie.favorite.add(request.user)
+
+    return redirect('/movies/{0}'.format(id))
+
+def remove_from_favorites(request, id):
+    movie = Movie.objects.get(id=id)
+    movie.favorite.remove(request.user)
+
+    return redirect('/movies/{0}'.format(id))
